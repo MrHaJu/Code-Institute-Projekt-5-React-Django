f461db30d0ad3f46fb3b1ff3d327b0bfaeb677ad
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-11-27 10:47:09 +0100

    add hamburger Icon to Navbar

--- food-app/src/components/Navbar.js
+++ food-app/src/components/Navbar.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
-export default function Navbar(){
-    return (
-        <div className="navbar container">
-            <a href="#!" className="logo">Mr.HaJu's <span>Recipes</span> sharing</a>
-            <div className="nav-links">
-                <a href="#!">Home</a>
-                <a href="#!">Recipes</a>
-                <a href="#!">Login</a>
-                <a href="#!">Register</a>
-                <a href="#!">Settings</a>
-            </div>
-        </div>
-    )
+export default function Navbar() {
+  const [showSidebar, setShowSidebar] = useState(false);
+  return (
+    <div className="navbar container">
+      <a href="#!" className="logo">
+        Mr.HaJu's <span>Recipes</span> sharing
+      </a>
+      <div className="nav-links">
+        <a href="#!">Home</a>
+        <a href="#!">Recipes</a>
+        <a href="#!">Login</a>
+        <a href="#!">Register</a>
+        <a href="#!">Settings</a>
+      </div>
+      <div
+        onClick={() => setShowSidebar(!showSidebar)}
+        className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+      >
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
 }
