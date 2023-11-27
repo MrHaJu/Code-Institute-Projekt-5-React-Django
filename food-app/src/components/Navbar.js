b67656df71d43e28268faed7f0ce76c95dbce4f3
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-11-27 13:37:20 +0100

    create sidebar with animated icons

--- food-app/src/components/Navbar.js
+++ food-app/src/components/Navbar.js
@@ -1,28 +1,68 @@
 import React from "react";
 import { useState } from "react";
+import Sidebar from "./sidebar";
+import {faHome, faList, faCog, faUser, faSignIn} from "@fortawesome/free-solid-svg-icons"
 
 export default function Navbar() {
-  const [showSidebar, setShowSidebar] = useState(false);
-  return (
-    <div className="navbar container">
-      <a href="#!" className="logo">
-        Mr.HaJu's <span>Recipes</span> sharing
-      </a>
-      <div className="nav-links">
-        <a href="#!">Home</a>
-        <a href="#!">Recipes</a>
-        <a href="#!">Login</a>
-        <a href="#!">Register</a>
-        <a href="#!">Settings</a>
-      </div>
-      <div
-        onClick={() => setShowSidebar(!showSidebar)}
-        className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
-      >
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-    </div>
+    const [showSidebar, setShowSidebar] = useState(false);
+    const links = [
+        {
+            name: "Home",
+            path: "/",
+            icon: faHome
+        },
+        {
+            name: "Recipes",
+            path: "/recipes",
+            icon: faList
+        },
+        {
+            name: "Login",
+            path: "/login",
+            icon: faUser
+        },
+        {
+            name: "Register",
+            path: "/register",
+            icon: faSignIn
+        },
+        {
+            name: "Settings",
+            path: "/settings",
+            icon: faCog
+        }
+    ]
+
+    function closeSidebar(){
+        setShowSidebar(false);
+    }
+
+    return (
+    <>
+        <div className="navbar container">
+            <a href="#!" className="logo">
+                Mr.HaJu's <span>Recipes</span> sharing
+            </a>
+            <div className="nav-links">
+                {links.map(link => (
+                    <a href="#!"/*{link.path}*/ key={link.name}>{link.name}</a>
+                ))}
+                {/*<a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">Login</a>
+                <a href="#!">Register</a>
+  <a href="#!">Settings</a>*/}
+            </div>
+            <div
+                onClick={() => setShowSidebar(!showSidebar)}
+                className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+            >
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+        </div>
+        { showSidebar && <Sidebar close={closeSidebar} links={links} /> }
+    </>
   );
 }
