3adca58e321c2e548c8de7bab36901f4873bc2dd
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-12-01 12:31:37 +0100

    create recipe crards

--- food-app/src/components/RecipeCard.js
+++ food-app/src/components/RecipeCard.js
@@ -1,7 +1,16 @@
-export default function RecipeCard() {
+import CustomImage from "./customimage"
+
+
+export default function RecipeCard({recipe}) {
     return(
         <div className="recipe-card">
-            Card
+            <CustomImage imgSrc={recipe.image} pt="65%"/>
+            <div className="recipe-card-info">
+                <img className="auther-img" src={recipe.authorImg} alt="Chef" />
+                <p className="recipe-title">{recipe.title}</p>
+                <p className="recipe-desc">{recipe.descript}</p>
+                <a className="view-btn" href={recipe.link}>View Recipe</a>
+            </div>
         </div>
     )
 }
--- food-app/src/pages/Recipes.js
+++ food-app/src/pages/Recipes.js
@@ -1,13 +1,72 @@
 import RecipeCard from "../components/RecipeCard"
 import PreviousSearches from "../components/previousSearches"
 export default function Recepies() {
-
+    const recipes = [
+        {
+            name: "Rezeptname",
+            image: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701428089/PP5/Rezept_bild_ax87td.png",
+            authorImg: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701427688/PP5/Autor_bild_nkd5e8.png",
+            title: "Rezeptname",
+            descript: "Beschreibung",
+            link: "Link zum Rezept"
+        },
+        {
+            name: "Rezeptname",
+            image: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701428089/PP5/Rezept_bild_ax87td.png",
+            authorImg: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701427688/PP5/Autor_bild_nkd5e8.png",
+            title: "Rezeptname",
+            descript: "Beschreibung",
+            link: "Link zum Rezept"
+        },
+        {
+            name: "Rezeptname",
+            image: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701428089/PP5/Rezept_bild_ax87td.png",
+            authorImg: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701427688/PP5/Autor_bild_nkd5e8.png",
+            title: "Rezeptname",
+            descript: "Beschreibung",
+            link: "Link zum Rezept"
+        },
+        {
+            name: "Rezeptname",
+            image: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701428089/PP5/Rezept_bild_ax87td.png",
+            authorImg: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701427688/PP5/Autor_bild_nkd5e8.png",
+            title: "Rezeptname",
+            descript: "Beschreibung",
+            link: "Link zum Rezept"
+        },
+        {
+            name: "Rezeptname",
+            image: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701428089/PP5/Rezept_bild_ax87td.png",
+            authorImg: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701427688/PP5/Autor_bild_nkd5e8.png",
+            title: "Rezeptname",
+            descript: "Beschreibung",
+            link: "Link zum Rezept"
+        },
+        {
+            name: "Rezeptname",
+            image: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701428089/PP5/Rezept_bild_ax87td.png",
+            authorImg: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701427688/PP5/Autor_bild_nkd5e8.png",
+            title: "Rezeptname",
+            descript: "Beschreibung",
+            link: "Link zum Rezept"
+        },
+        {
+            name: "Rezeptname",
+            image: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701428089/PP5/Rezept_bild_ax87td.png",
+            authorImg: "https://res.cloudinary.com/db6t1xmmn/image/upload/c_scale,w_550/v1701427688/PP5/Autor_bild_nkd5e8.png",
+            title: "Rezeptname",
+            descript: "Beschreibung",
+            link: "Link zum Rezept"
+        },
+    ].sort(() => Math.random() - 0.5)
     return (
         <div>
             <PreviousSearches />
             <div className="recipes-container">
-                <RecipeCard />
-
+                {recipes.map((recipe, index) => (
+                    <RecipeCard key={index} recipe={recipe} />
+                ))}
+                
             </div>
         </div>
     )
