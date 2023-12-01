c23dfc79c951b584595260ca3e89e229a9ae4a39
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-12-01 10:47:42 +0100

    change from equality to strict operators

--- food-app/src/components/Navbar.js
+++ food-app/src/components/Navbar.js
@@ -54,7 +54,7 @@ export default function Navbar() {
         <div className="nav-links">
           {links.map((link) => (
             <Link
-            className={location.pathname == link.path ? "active" : ""}
+            className={location.pathname === link.path ? "active" : ""}
               to={link.path}
               key={link.name}
             >
