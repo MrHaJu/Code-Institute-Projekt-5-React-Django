e79c47ea57ea3caec7c69a92bc78371427f1b218
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-12-05 13:13:07 +0100

    create login section

--- food-app/src/pages/Login.js
+++ food-app/src/pages/Login.js
@@ -1,7 +1,52 @@
-export default function Login() {
-    return (
-        <main>
-            <h1>Login</h1>
-        </main>
-    )
-}
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    usernameOrEmail: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Hier kannst du die Login-Logik implementieren
+    // Zum Beispiel: Aufruf einer API, die einen Benutzer einloggt
+    console.log('Login-Daten:', formData);
+  };
+
+  return (
+  <main>
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="usernameOrEmail">Username or E-Mail:</label>
+        <input
+          type="text"
+          id="usernameOrEmail"
+          name="usernameOrEmail"
+          value={formData.usernameOrEmail}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
+        <button className="btn" type="submit">Login</button>
+      </form>
+    </div>
+    </main>
+  );
+};
+
+export default Login;
