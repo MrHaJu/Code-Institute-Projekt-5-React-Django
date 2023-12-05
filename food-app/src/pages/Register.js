30597cc03091a9f441b3b6dacfef3ab6d35c6d2a
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-12-05 13:14:03 +0100

    format document

--- food-app/src/pages/Register.js
+++ food-app/src/pages/Register.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Register = () => {
   const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   });
 
   const handleChange = (e) => {
@@ -16,57 +16,59 @@ const Register = () => {
     e.preventDefault();
     // Hier kannst du die Registrierungslogik implementieren
     // Zum Beispiel: Aufruf einer API, die einen Benutzer registriert
-    console.log('Registrierungsdaten:', formData);
+    console.log("Registrierungsdaten:", formData);
   };
 
   return (
     <main>
-    <div className="register-container">
-      <h2>Sign up</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          required
-        />
+      <div className="register-container">
+        <h2>Sign up</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
 
-        <label htmlFor="email">E-Mail:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
+          <label htmlFor="email">E-Mail:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
 
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
 
-        <label htmlFor="confirmPassword">confirm password:</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          required
-        />
+          <label htmlFor="confirmPassword">confirm password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
 
-        <button className='btn' type="submit">Sign up</button>
-      </form>
-    </div>
+          <button className="btn" type="submit">
+            Sign up
+          </button>
+        </form>
+      </div>
     </main>
   );
 };
