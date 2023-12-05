dd609a09104dd8831f83fba8ef97d021065aaecd
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-12-05 13:13:20 +0100

    create signup section

--- food-app/src/pages/Register.js
+++ food-app/src/pages/Register.js
@@ -1,7 +1,74 @@
-export default function Register() {
-    return (
-        <main>
-            <h1>Sign Up</h1>
-        </main>
-    )
-}
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Hier kannst du die Registrierungslogik implementieren
+    // Zum Beispiel: Aufruf einer API, die einen Benutzer registriert
+    console.log('Registrierungsdaten:', formData);
+  };
+
+  return (
+    <main>
+    <div className="register-container">
+      <h2>Sign up</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="email">E-Mail:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="confirmPassword">confirm password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+
+        <button className='btn' type="submit">Sign up</button>
+      </form>
+    </div>
+    </main>
+  );
+};
+
+export default Register;
